b941c558fff1d45d2f012038f56ceb4404bb143c
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2022-09-13 14:17:07 +0100

    dark mode

--- script.js
+++ script.js
@@ -22,17 +22,10 @@ function setTheme(mode){
 		document.getElementById('theme-style').href = 'default.css'
 	}
 
-	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
+	if(mode == 'dark'){
+		document.getElementById('theme-style').href = 'dark.css'
 	}
 
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
-	}
-
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-	}
 
 	localStorage.setItem('theme', mode)
 }
