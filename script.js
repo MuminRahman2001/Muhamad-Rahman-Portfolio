0255b775b55ed9125d5d5311aff5440cdb7cc851
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2023-06-12 14:42:36 +0100

    update cv

--- script.js
+++ script.js
@@ -29,3 +29,53 @@ function setTheme(mode){
 
 	localStorage.setItem('theme', mode)
 }
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'dark'){
+		document.getElementById('theme-style').href = 'dark.css'
+	}
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'dark'){
+		document.getElementById('theme-style').href = 'dark.css'
+	}
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'dark'){
+		document.getElementById('theme-style').href = 'dark.css'
+	}
