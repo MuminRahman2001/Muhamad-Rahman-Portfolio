806c2da3e4b6ab03b93fb424dbdde1056604dc9e
MuminRahman2001 <65430144+MuminRahman2001@users.noreply.github.com>
2023-09-14 17:38:41 +0100

    Update script.js
    fixed error for theme setting and saving and reduction of code.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let theme = localStorage.getItem('theme')
+let theme = localStorage.getItem('theme');
 
 if(theme == null){
 	setTheme('light')
@@ -6,13 +6,13 @@ if(theme == null){
 	setTheme(theme)
 }
 
+
 let themeDots = document.getElementsByClassName('theme-dot')
 
 
 for (var i=0; themeDots.length > i; i++){
 	themeDots[i].addEventListener('click', function(){
 		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
 		setTheme(mode)
 	})
 }
@@ -28,57 +28,4 @@ function setTheme(mode){
 
 
 	localStorage.setItem('theme', mode)
-}
-
-
-for (var i=0; themeDots.length > i; i++){
-	themeDots[i].addEventListener('click', function(){
-		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
-}
-
-function setTheme(mode){
-	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
-	}
-
-	if(mode == 'dark'){
-		document.getElementById('theme-style').href = 'dark.css'
-	}
-	for (var i=0; themeDots.length > i; i++){
-		themeDots[i].addEventListener('click', function(){
-			let mode = this.dataset.mode
-			console.log('Option clicked:', mode)
-			setTheme(mode)
-		})
-	}
-}
-
-function setTheme(mode){
-	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
-	}
-
-	if(mode == 'dark'){
-		document.getElementById('theme-style').href = 'dark.css'
-	}
-	for (var i=0; themeDots.length > i; i++){
-		themeDots[i].addEventListener('click', function(){
-			let mode = this.dataset.mode
-			console.log('Option clicked:', mode)
-			setTheme(mode)
-		})
-	}
-}
-
-function setTheme(mode){
-	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
-	}
-
-	if(mode == 'dark'){
-		document.getElementById('theme-style').href = 'dark.css'
-	}
 }
